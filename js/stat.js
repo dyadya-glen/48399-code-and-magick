9e07d2e7f541a668a29a36814b6e06a8423a1af2
Roman Gulyaev <glen2001@yandex.ru>
2017-01-23 22:42:05 +0300

    Задание гистограмма 2я проверка

--- js/stat.js
+++ js/stat.js
@@ -1,58 +1,87 @@
 'use strict';
 
-var drowCloud = function (ctx, x, y, width, height) {
+function drawRectangle (ctx, x, y, width, height) {
+  var borderRadius = 20;
   ctx.beginPath ();
-  ctx.moveTo (x + 20, y);
-  ctx.lineTo ((x + width) - 20, y);
-  ctx.quadraticCurveTo (x + width, y, x + width, y + 20);
-  ctx.lineTo (x + width, (y + height) - 20);
-  ctx.quadraticCurveTo (x + width, y + height, (x + width) - 20, y + height);
-  ctx.lineTo (x + 20, y + height);
-  ctx.quadraticCurveTo (x, y + height, x, (y + height) - 20);
-  ctx.lineTo (x, y + 20);
+  ctx.moveTo (x + borderRadius, y);
+  ctx.lineTo (x + width - borderRadius, y);
+  ctx.quadraticCurveTo (
+    x + width,
+    y,
+    x + width,
+    y + borderRadius
+  );
+  ctx.lineTo (x + width, y + height - borderRadius);
+  ctx.quadraticCurveTo (
+    x + width,
+    y + height,
+    x + width - borderRadius,
+    y + height
+  );
+  ctx.lineTo (x + borderRadius, y + height);
+  ctx.quadraticCurveTo (
+    x,
+    y + height,
+    x,
+    y + height - borderRadius
+  );
+  ctx.lineTo (x, y + borderRadius);
   ctx.stroke ();
   ctx.fill ();
 };
 
+function histogramRender (ctx, names, times) {
+  var maxTtime =  Math.max.apply(Math, times);
+
+  for (var i = 0 ; i < times.length; i++) {
+    var time = times[i];
+    var name = names[i];
+    var histogramHeight = 150;
+    var histogramWidth = 40;
+    var histogramX = 155;
+    var columnIndent = 50;
+    var height = histogramHeight / maxTtime * time;
+
+    ctx.fillText (
+      time.toFixed(0),
+      histogramX + i * (histogramWidth + columnIndent),
+      histogramHeight + 90 - height
+    );
+
+    if (name === 'Вы') {
+      ctx.fillStyle = '#f00';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0,'  + (Math.random() * 5).toFixed(0) * 50 +  ',' + Math.random() + ')';
+    }
+
+    ctx.fillRect (
+      histogramX + i * (histogramWidth + columnIndent),
+      100 + histogramHeight, histogramWidth, -height
+    );
+
+    ctx.fillStyle = '#000';
+
+    ctx.fillText (
+      name,
+      histogramX + i * (histogramWidth + columnIndent),
+      100 + histogramHeight + 20
+    );
+  }
+}
+
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.strokeStyle = 'transparent';
-  drowCloud (ctx, 110, 20, 420, 270);
+  drawRectangle (ctx, 110, 20, 420, 270);
 
   ctx.fillStyle = 'white';
   ctx.strokeStyle = 'white';
-  drowCloud (ctx, 100, 10, 420, 270);
+  drawRectangle (ctx, 100, 10, 420, 270);
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.fillText ('Ура вы победили!', 120, 40);
   ctx.fillText ('Список результатов:', 120, 60);
 
-  var max = -1;
-
-  for (var i = 0 ; i < times.length; i++) {
-    var time = times[i];
-    var name = names[i];
-    var histoHeight = 150;
-    var histoWidth = 40;
-    var histoX = 155;
-    var columnIndent = 50;
-
-    if (time > max) {
-      max = time;
-    }
-
-    var step = histoHeight / max;
-    var height = step * time;
-
-    ctx.fillText (time.toFixed(0), histoX + i * (histoWidth + columnIndent), (histoHeight + 90) - height);
-    if (name === 'Вы') {
-      ctx.fillStyle = '#f00';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0,'  + (Math.random() * 5).toFixed(0) * 50 +  ',' + Math.random() + ')';
-    }
-    ctx.fillRect (histoX + i * (histoWidth + columnIndent), 100 + histoHeight, histoWidth, -height);
-    ctx.fillStyle = '#000';
-    ctx.fillText (name, histoX + i * (histoWidth + columnIndent), 100 + histoHeight + 20);
-  }
+  histogramRender (ctx, names, times);
 };
