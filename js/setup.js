3bfc0778e1fe7c290afeead6f57a097b250c6d3e
Roman Gulyaev <glen2001@yandex.ru>
2017-02-04 23:45:48 +0300

    #13 Учебный проект: Нажатие посохом v1.0

--- js/setup.js
+++ js/setup.js
@@ -2,11 +2,13 @@
 
 var setupWizard = document.querySelector('.setup');
 var setupOpenWizard = document.querySelector('.setup-open');
+var setupOpenIcon = setupOpenWizard.querySelector('.setup-open-icon');
 var setupCloseWizard = setupWizard.querySelector('.setup-close');
 var userName = setupWizard.querySelector('.setup-user-name');
 var wizardCoat = document.getElementById('wizard-coat');
 var wizardEyes = document.getElementById('wizard-eyes');
 var setupFireball = setupWizard.querySelector('.setup-fireball-wrap');
+var setupSubmit = setupWizard.querySelector('.setup-submit');
 var indexColor = 0;
 var wizardCoatColors = [
   'rgb(101, 137, 164)',
@@ -33,24 +35,78 @@ var fireballColors = [
   '#e6e848'
 ];
 
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
 userName.required = true;
 userName.maxLength = 50;
 
-setupOpenWizard.addEventListener('click', function () {
+setupOpenIcon.addEventListener('focus', onFocusSetupOpenIcon);
+setupOpenIcon.addEventListener('blur', onBlurSetupOpenIcon);
+setupCloseWizard.addEventListener('focus', onFocusSetupCloseWizard);
+setupCloseWizard.addEventListener('blur', onBlurSetupCloseWizard);
+setupOpenWizard.addEventListener('click', onShowSetupWizard);
+setupOpenWizard.addEventListener('keydown', onPressShowSetupWizard);
+setupCloseWizard.addEventListener('click', onHideSetupWizard);
+setupCloseWizard.addEventListener('keydown', onPressHideSetupWizard);
+wizardCoat.addEventListener('click', onChoiceOfColorCoat);
+wizardEyes.addEventListener('click', onChoiceOfColorEyes);
+setupFireball.addEventListener('click', onChoiceOfColorFireball);
+setupSubmit.addEventListener('click', onHideSetupWizard);
+
+function onFocusSetupOpenIcon() {
+  setupOpenIcon.setAttribute('aria-pressed', true);
+}
+
+function onBlurSetupOpenIcon() {
+  setupOpenIcon.setAttribute('aria-pressed', false);
+}
+
+function onFocusSetupCloseWizard() {
+  setupCloseWizard.setAttribute('aria-pressed', true);
+}
+
+function onBlurSetupCloseWizard() {
+  setupCloseWizard.setAttribute('aria-pressed', false);
+}
+
+function isActivateEvent(event) {
+  return event.keyCode && event.keyCode === ENTER_KEY_CODE;
+}
+
+function setupKeydownHendler(event) {
+  if (event.keyCode === ESCAPE_KEY_CODE) {
+    setupWizard.classList.add('invisible');
+    setupWizard.setAttribute('aria-hidden', true);
+  }
+}
+
+function onShowSetupWizard(event) {
   setupWizard.classList.remove('invisible');
-});
+  setupWizard.setAttribute('aria-hidden', false);
+  document.addEventListener('keydown', setupKeydownHendler);
+}
 
-setupCloseWizard.addEventListener('click', function () {
+function onHideSetupWizard(event) {
+  event.preventDefault();
   setupWizard.classList.add('invisible');
-});
+  setupWizard.setAttribute('aria-hidden', true);
+  document.removeEventListener('keydown', setupKeydownHendler);
+}
 
-wizardCoat.addEventListener('click', choiceOfColorCoat);
+function onPressShowSetupWizard(event) {
+  if (isActivateEvent(event)) {
+    onShowSetupWizard(event);
+  }
+}
 
-wizardEyes.addEventListener('click', choiceOfColorEyes);
+function onPressHideSetupWizard(event) {
+  if (isActivateEvent(event)) {
+    onHideSetupWizard();
+  }
+}
 
-setupFireball.addEventListener('click', choiceOfColorFireball);
-
-function choiceOfColorCoat() {
+function onChoiceOfColorCoat() {
   var nextIndexColor = ++indexColor;
   if (!wizardCoatColors[nextIndexColor]) {
     nextIndexColor = 0;
@@ -59,7 +115,7 @@ function choiceOfColorCoat() {
   indexColor = nextIndexColor;
 }
 
-function choiceOfColorEyes() {
+function onChoiceOfColorEyes() {
   var nextIndexColor = ++indexColor;
   if (!wizardEyesColors[nextIndexColor]) {
     nextIndexColor = 0;
@@ -68,7 +124,7 @@ function choiceOfColorEyes() {
   indexColor = nextIndexColor;
 }
 
-function choiceOfColorFireball() {
+function onChoiceOfColorFireball() {
   var colorIndex = Math.floor(Math.random() * fireballColors.length);
   setupFireball.style.backgroundColor = fireballColors[colorIndex];
 }
