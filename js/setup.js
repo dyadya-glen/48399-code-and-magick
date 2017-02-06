dc928d0209fd7606501c7f3dc00a5ccf5c5a4315
dyadya-glen <glen2001@yandex.ru>
2017-02-06 20:46:40 +0300

    Учебный проект: Нажатие посохом (#4)
    * #13 Учебный проект: Нажатие посохом v1.0
    
    * #13 Учебный проект: Нажатие посохом v1.1
    
    * #13 Исправлял ошибки v1.2
    
    * #13 И еще раз исправлял ошибки :) v1.3

--- js/setup.js
+++ js/setup.js
@@ -7,6 +7,7 @@ var userName = setupWizard.querySelector('.setup-user-name');
 var wizardCoat = document.getElementById('wizard-coat');
 var wizardEyes = document.getElementById('wizard-eyes');
 var setupFireball = setupWizard.querySelector('.setup-fireball-wrap');
+var setupSubmit = setupWizard.querySelector('.setup-submit');
 var indexColor = 0;
 var wizardCoatColors = [
   'rgb(101, 137, 164)',
@@ -33,24 +34,71 @@ var fireballColors = [
   '#e6e848'
 ];
 
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
 userName.required = true;
 userName.maxLength = 50;
 
-setupOpenWizard.addEventListener('click', function () {
+setupOpenWizard.addEventListener('click', onShowSetupWizard);
+setupOpenWizard.addEventListener('keydown', onPressShowSetupWizard);
+setupCloseWizard.addEventListener('click', onHideSetupWizard);
+setupCloseWizard.addEventListener('keydown', onPressHideSetupWizard);
+wizardCoat.addEventListener('click', onChoiceCoatColor);
+wizardEyes.addEventListener('click', onChangesEyesColor);
+setupFireball.addEventListener('click', onChoiceOfColorFireball);
+setupSubmit.addEventListener('click', onSaveSetupWizard);
+setupSubmit.addEventListener('keydown', onPressSaveSetupWizard);
+
+function isActivateEvent(event) {
+  return event.keyCode && event.keyCode === ENTER_KEY_CODE;
+}
+
+function setupKeydownHendler(event) {
+  if (event.keyCode === ESCAPE_KEY_CODE) {
+    onHideSetupWizard();
+  }
+}
+
+function onShowSetupWizard(event) {
   setupWizard.classList.remove('invisible');
-});
+  setupWizard.setAttribute('aria-hidden', false);
+  setupOpenWizard.setAttribute('aria-pressed', true);
+  document.addEventListener('keydown', setupKeydownHendler);
+}
 
-setupCloseWizard.addEventListener('click', function () {
+function onHideSetupWizard() {
   setupWizard.classList.add('invisible');
-});
+  setupWizard.setAttribute('aria-hidden', true);
+  setupOpenWizard.setAttribute('aria-pressed', false);
+  document.removeEventListener('keydown', setupKeydownHendler);
+}
 
-wizardCoat.addEventListener('click', choiceOfColorCoat);
+function onPressShowSetupWizard(event) {
+  if (isActivateEvent(event)) {
+    onShowSetupWizard(event);
+  }
+}
 
-wizardEyes.addEventListener('click', choiceOfColorEyes);
+function onPressHideSetupWizard(event) {
+  if (isActivateEvent(event)) {
+    onHideSetupWizard();
+  }
+}
 
-setupFireball.addEventListener('click', choiceOfColorFireball);
+function onSaveSetupWizard(event) {
+  event.preventDefault();
+  onHideSetupWizard();
+}
 
-function choiceOfColorCoat() {
+function onPressSaveSetupWizard(event) {
+  event.preventDefault();
+  if (isActivateEvent(event)) {
+    onHideSetupWizard();
+  }
+}
+
+function onChoiceCoatColor() {
   var nextIndexColor = ++indexColor;
   if (!wizardCoatColors[nextIndexColor]) {
     nextIndexColor = 0;
@@ -59,7 +107,7 @@ function choiceOfColorCoat() {
   indexColor = nextIndexColor;
 }
 
-function choiceOfColorEyes() {
+function onChangesEyesColor() {
   var nextIndexColor = ++indexColor;
   if (!wizardEyesColors[nextIndexColor]) {
     nextIndexColor = 0;
@@ -68,7 +116,7 @@ function choiceOfColorEyes() {
   indexColor = nextIndexColor;
 }
 
-function choiceOfColorFireball() {
+function onChoiceOfColorFireball() {
   var colorIndex = Math.floor(Math.random() * fireballColors.length);
   setupFireball.style.backgroundColor = fireballColors[colorIndex];
 }
